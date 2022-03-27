846c43bce04962c7ecb9cf7641b5ccd1b166d07c
Drijon7577 <87547203+Drijon7577@users.noreply.github.com>
2022-03-27 10:05:59 +0200

    Update 500.jsx

--- pages/500.jsx
+++ pages/500.jsx
@@ -6,10 +6,11 @@ export default class Error500 extends React.Component {
         return (
             <>
                 <Head>
-                    <title>Luna - 500</title>
+                    <title>Drijon - 500</title>
                 </Head>
                 <div className={style.align}>
                     <div className={style.container}>
+                        <img src='https://http.cat/404.jpeg' style={{ width: '100%' }} />
                         <strong style={{ fontSize: 32, color: '#ddd9e6' }}>500</strong><text style={{ color: 'rgb(65, 59, 74)', fontSize: 24 }}> ⌋ Internal server error</text> <br />
                     </div>
                 </div>
