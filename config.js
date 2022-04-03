f2cec731f3d76207ace70ba182863358cea3fbe6
Drijon7577 <87547203+Drijon7577@users.noreply.github.com>
2022-04-03 13:43:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
         username: "</> Drijon",
         discriminator: "7577",
         nickname: "Drijon H.",
-        nickavatar: "https://cdn.discordapp.com/avatars/778557320123777026/b5a47116b4f465d7de313b4ca9a50895.png?size=4096",
+        nickavatar: "https://us-east-1.tixte.net/uploads/cdn.drijon.tk/chrome_bMJm0pToow.png",
         status: {
             state: {
                 text: "online",
@@ -19,7 +19,7 @@ module.exports = {
         },
         activities: [],
         createdTimestamp: 1615924806867,
-        avatar: "https://cdn.discordapp.com/avatars/778557320123777026/b5a47116b4f465d7de313b4ca9a50895.png?size=4096",
+        avatar: "https://us-east-1.tixte.net/uploads/cdn.drijon.tk/chrome_bMJm0pToow.png",
         banner: "https://cdn.discordapp.com/attachments/957388729830486116/957974761722478732/IMG_2282.webp",
         accentColor: "#0EEFD5"
     },
